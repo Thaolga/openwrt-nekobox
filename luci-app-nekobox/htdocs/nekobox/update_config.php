f88b8454d2034cc833454644293a241f390fb31d
Auroraxpm <73517150+Auroraxpm@users.noreply.github.com>
2024-10-26 20:11:27 +0800

    Update update_config.php

--- luci-app-nekobox/htdocs/nekobox/update_config.php
+++ luci-app-nekobox/htdocs/nekobox/update_config.php
@@ -8,20 +8,25 @@ function logMessage($message) {
     file_put_contents($logFile, "[$timestamp] $message\n", FILE_APPEND);
 }
 
-$download_url = "https://raw.githubusercontent.com/Thaolga/openwrt-nekobox/nekobox/luci-app-nekobox/root/etc/neko/config/mihomo.yaml";
-$destination_path = "/etc/neko/config/mihomo.yaml";
+$urls = [
+    "https://raw.githubusercontent.com/Thaolga/openwrt-nekobox/nekobox/luci-app-nekobox/root/etc/neko/config/mihomo.yaml" => "/etc/neko/config/mihomo.yaml",
+    "https://raw.githubusercontent.com/Thaolga/openwrt-nekobox/nekobox/luci-app-nekobox/root/etc/neko/config/Puernya.json" => "/etc/neko/config/Puernya.json"
+];
 
-if (!is_dir(dirname($destination_path))) {
-    mkdir(dirname($destination_path), 0755, true);
+foreach ($urls as $download_url => $destination_path) {
+    if (!is_dir(dirname($destination_path))) {
+        mkdir(dirname($destination_path), 0755, true);
+    }
+
+    exec("wget -O '$destination_path' '$download_url'", $output, $return_var);
+    if ($return_var !== 0) {
+        logMessage("下载失败：$destination_path");
+        die("下载失败：$destination_path");
+    }
+
+    logMessage(basename($destination_path) . " 文件已成功更新！");
 }
 
-exec("wget -O '$destination_path' '$download_url'", $output, $return_var);
-if ($return_var !== 0) {
-    logMessage("下载失败！");
-    die("下载失败！");
-}
-
-logMessage("mihomo.yaml 文件已成功更新！");
 echo "文件已成功更新！";
 
 ?>
