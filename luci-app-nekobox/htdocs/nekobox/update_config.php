0acf9c7271d2baa0e0632ebc542b58bd092df897
Auroraxpm <73517150+Auroraxpm@users.noreply.github.com>
2024-10-30 22:08:56 +0800

    Update update_config.php

--- luci-app-nekobox/htdocs/nekobox/update_config.php
+++ luci-app-nekobox/htdocs/nekobox/update_config.php
@@ -6,7 +6,7 @@ $logMessages = [];
 
 function logMessage($message) {
     global $logMessages;
-    $timestamp = date('H:i:s');
+    $timestamp = date('H:i:s', strtotime('+8 hours'));
     $logMessages[] = "[$timestamp] $message";
 }
 
@@ -15,18 +15,28 @@ $urls = [
     "https://mirror.ghproxy.com/https://raw.githubusercontent.com/Thaolga/openwrt-nekobox/nekobox/luci-app-nekobox/root/etc/neko/config/Puernya.json" => "/etc/neko/config/Puernya.json"
 ];
 
+function downloadFile($url, $path, $retries = 3) {
+    for ($i = 0; $i < $retries; $i++) {
+        $command = "curl -L --fail -o '$path' '$url'";
+        exec($command, $output, $return_var);
+
+        if ($return_var === 0) {
+            logMessage(basename($path) . " 文件已成功更新！");
+            return true;
+        } else {
+            logMessage("下载失败：$path，重试中（" . ($i + 1) . "/$retries）...");
+            sleep(2);  
+        }
+    }
+    logMessage("下载失败：$path，已超过最大重试次数！");
+    return false;
+}
+
 foreach ($urls as $download_url => $destination_path) {
     if (!is_dir(dirname($destination_path))) {
         mkdir(dirname($destination_path), 0755, true);
     }
-
-    exec("wget -O '$destination_path' '$download_url'", $output, $return_var);
-    if ($return_var !== 0) {
-        logMessage("Download failed: $destination_path");
-        die("Download failed: $destination_path");
-    }
-
-    logMessage(basename($destination_path) . " file has been successfully updated!");
+    downloadFile($download_url, $destination_path);
 }
 
 echo implode("\n", $logMessages);
