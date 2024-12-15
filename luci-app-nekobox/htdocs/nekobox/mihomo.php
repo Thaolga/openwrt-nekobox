b111fc564e52167d01c06dc466ac1fe4713f61ab
Auroraxpm <73517150+Auroraxpm@users.noreply.github.com>
2024-12-15 22:56:00 +0800

    Update mihomo.php

--- luci-app-nekobox/htdocs/nekobox/mihomo.php
+++ luci-app-nekobox/htdocs/nekobox/mihomo.php
@@ -859,7 +859,7 @@ function getSubscriptionUrlFromFile($file) {
         <div class="help mt-4">
             <p style="color: red;">注意：在线订阅转换存在隐私泄露风险</p>
             <p>订阅转换由肥羊提供</p>
-            <a href="https://github.com/youshandefeiyang/sub-web-modify" target="_blank" style="display: inline-block; padding: 10px 20px; background-color: #007bff; color: #fff; text-decoration: none; border-radius: 5px; font-size: 16px;">
+            <a href="https://github.com/youshandefeiyang/sub-web-modify" target="_blank" class="btn btn-primary">
             点击访问
             </a>
         </div>
