81941172ed14953a9c0ad59e76e08b04df83a3cd
Auroraxpm <73517150+Auroraxpm@users.noreply.github.com>
2025-03-26 13:39:13 +0800

    Optimize page layout

--- luci-theme-spectra/luasrc/controller/spectra.lua
+++ luci-theme-spectra/luasrc/controller/spectra.lua
@@ -1,5 +1,7 @@
 module("luci.controller.spectra", package.seeall)
 
 function index()
-    entry({"admin", "services", "spectra"}, template("spectra/index"), _("Spectra 主题设置"), 1)
+    entry({"admin", "services", "spectra"}, firstchild(), _("Spectra 主题设置"), 1).leaf = false
+    entry({"admin", "services", "spectra", "index"}, template("spectra/index"), _("首页"), 2).leaf = true
+    entry({"admin", "services", "spectra", "filekit"}, template("spectra/filekit"), _("文件助手"), 3).leaf = true
 end
