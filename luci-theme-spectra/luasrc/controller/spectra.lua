7ea02d7d9b49011f7151c6e72069becd4b06edea
Auroraxpm <73517150+Auroraxpm@users.noreply.github.com>
2026-01-28 20:45:46 +0800

    v27.558

--- luci-theme-spectra/luasrc/controller/spectra.lua
+++ luci-theme-spectra/luasrc/controller/spectra.lua
@@ -3,6 +3,7 @@ module("luci.controller.spectra", package.seeall)
 function index()
     entry({"admin", "services", "spectra"}, firstchild(), _("Spectra"), 1).leaf = false
     entry({"admin", "services", "spectra", "index"}, template("spectra/index"), _("Home"), 2).leaf = true
-    entry({"admin", "services", "spectra", "main"}, template("spectra/main"), _("Music"), 3).leaf = true
-    entry({"admin", "services", "spectra", "filekit"}, template("spectra/filekit"), _("Manager"), 4).leaf = true
+    entry({"admin", "services", "spectra", "media"}, template("spectra/media"), _("Media"), 3).leaf = true
+    entry({"admin", "services", "spectra", "main"}, template("spectra/main"), _("Music"), 4).leaf = true
+    entry({"admin", "services", "spectra", "filekit"}, template("spectra/filekit"), _("Manager"), 5).leaf = true
 end
