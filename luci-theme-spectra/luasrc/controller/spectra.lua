9bf07759a648c31673b6f9b70aa8431b6b6e57b6
Auroraxpm <73517150+Auroraxpm@users.noreply.github.com>
2026-02-11 22:15:30 +0800

    Update spectra.lua

--- luci-theme-spectra/luasrc/controller/spectra.lua
+++ luci-theme-spectra/luasrc/controller/spectra.lua
@@ -1,9 +1,35 @@
 module("luci.controller.spectra", package.seeall)
 
 function index()
-    entry({"admin", "services", "spectra"}, firstchild(), _("Spectra"), 1).leaf = false
-    entry({"admin", "services", "spectra", "index"}, template("spectra/index"), _("Home"), 2).leaf = true
-    entry({"admin", "services", "spectra", "media"}, template("spectra/media"), _("Media"), 3).leaf = true
-    entry({"admin", "services", "spectra", "main"}, template("spectra/main"), _("Music"), 4).leaf = true
-    -- entry({"admin", "services", "spectra", "filekit"}, template("spectra/filekit"), _("Manager"), 5).leaf = true
+    entry({"admin", "spectra"},
+        firstchild(),
+        _("Spectra"),
+        41
+    )
+
+    entry({"admin", "spectra", "index"}, 
+        template("spectra/index"),
+        _("Home"), 
+        2
+    ).leaf = true
+    
+    entry({"admin", "spectra", "media"}, 
+        template("spectra/media"),
+        _("Media"), 
+        3
+    ).leaf = true
+    
+    entry({"admin", "spectra", "main"}, 
+        template("spectra/main"),
+        _("Music"), 
+        4
+    ).leaf = true
+    
+    --[[
+    entry({"admin", "spectra", "filekit"}, 
+        template("spectra/filekit"),
+        _("Manager"), 
+        5
+    ).leaf = true
+    --]]
 end
