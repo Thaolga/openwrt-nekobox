3cbc6af4425b3d7e636664de841fca214068d77b
Auroraxpm <73517150+Auroraxpm@users.noreply.github.com>
2026-02-15 17:59:03 +0800

    v27.566

--- luci-theme-spectra/root/www/spectra/stream/index.php
+++ luci-theme-spectra/root/www/spectra/stream/index.php
@@ -12895,9 +12895,13 @@ function updateBatchRenameFileList() {
 function removeSpecialCharsFromName(name) {
     const emojiRegex = /[\u{1F600}-\u{1F64F}|\u{1F300}-\u{1F5FF}|\u{1F680}-\u{1F6FF}|\u{1F700}-\u{1F77F}|\u{1F780}-\u{1F7FF}|\u{1F800}-\u{1F8FF}|\u{1F900}-\u{1F9FF}|\u{1FA00}-\u{1FA6F}|\u{1FA70}-\u{1FAFF}|\u{2600}-\u{26FF}|\u{2700}-\u{27BF}|\u{2B00}-\u{2BFF}|\u{2E00}-\u{2E7F}]/gu;
     name = name.replace(emojiRegex, '');
+    
     name = name.replace(/[#\s]/g, '_');
+    
     name = name.replace(/_+/g, '_');
+    
     name = name.replace(/^_+|_+$/g, '');
+    
     return name;
 }
 
